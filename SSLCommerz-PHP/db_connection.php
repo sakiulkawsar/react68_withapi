c13dbc49d35c8edb54731875014a8b2b1f21e806
Md sakiul kawsar <mr.sakiulkawsar@gmail.com>
2025-12-27 13:26:50 +0600

    m

---
+++ SSLCommerz-PHP/db_connection.php
@@ -0,0 +1,16 @@
+<?php
+
+$servername = "localhost";
+$username = "root"; // Put the MySQL Username
+$password = ""; // Put the MySQL Password
+$database = "sslcommerz";
+
+
+// Create connection for integration
+$conn_integration = mysqli_connect($servername, $username, $password, $database);
+
+// Check connection for integration
+if (!$conn_integration) {
+    die("Connection failed: " . mysqli_connect_error());
+}
+
